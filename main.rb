04009156e0d9472995e0167e02e2c9faff4f111c
icetimux <icetimux@gmail.com>
2021-04-06 21:09:24 +0200

    add customizable name

--- main.rb
+++ main.rb
@@ -9,13 +9,17 @@ require_relative 'helpers'
 Sequel.connect(ENV['DATABASE_URL'])
 require_relative 'models/paste'
 
+APP_NAME = ENV['NAME'] || 'PasteBin'
+
 get '/' do
+  @name = APP_NAME
   protected!
   @pastes = Paste.reverse_order(:created_at).all
   erb :index
 end
 
 get '/new' do
+  @name = APP_NAME
   protected!
   erb :new
 end
@@ -26,9 +30,13 @@ post '/create' do
   body =  params['body']
   created_at = db_format(Time.now)
   updated_at = created_at
-  Paste.insert(title: title,
-    body: body, created_at: created_at,
-    updated_at: updated_at)
+  paste = {
+    title: title,
+    body: body,
+    created_at: created_at,
+    updated_at: updated_at
+  }
+  Paste.insert(paste)
   paste = Paste.last
   redirect "/#{paste.id}"
 end
@@ -46,21 +54,24 @@ post '/update/:paste_id' do
 end
 
 get '/edit/:paste_id' do
+  @name = APP_NAME
   protected!
   @paste = Paste[params['paste_id']]
   erb :edit
 end
 
 get '/raw/:paste_id' do
+  @name = APP_NAME
   protected!
   @paste = Paste[params['paste_id']]
   "<pre>#{@paste.body}</pre>"
 end
 
 get '/markdown/:paste_id' do
+  @name = APP_NAME
   protected!
   @paste = Paste[params['paste_id']]
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+  settings = {
     autolink: true,
     tables: true,
     fenced_code_blocks: true,
@@ -68,7 +79,9 @@ get '/markdown/:paste_id' do
     superscript: true,
     underline: true,
     highlight: true,
-    footnotes: true)
+    footnotes: true
+  }
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, settings)
   @paste.body = markdown.render(@paste.body)
   erb :markdown
 end
@@ -85,6 +98,7 @@ get '/logout' do
 end
 
 get '/:paste_id' do
+  @name = APP_NAME
   protected!
   @paste = Paste[params['paste_id']]
   erb :show
